3b83a9f96c933c162d8a13b3318956bc43e2ba32
Camilo Vargas <camilovargasingm@gmail.com>
2022-11-19 09:32:48 -0500

    config redux

---
+++ src/redux/slices/index.ts
@@ -0,0 +1,11 @@
+
+import { combineReducers } from '@reduxjs/toolkit';
+
+
+const rootReducer = combineReducers({
+  
+})
+
+export type RootState = ReturnType<typeof rootReducer>
+
+export default rootReducer;
