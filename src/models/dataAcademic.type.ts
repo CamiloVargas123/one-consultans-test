dc0216c7589dbb854117e46df58051fb3629d5e7
Camilo Vargas <camilovargasingm@gmail.com>
2022-11-19 11:30:11 -0500

    update title academic in model

--- src/models/dataAcademic.type.ts
+++ src/models/dataAcademic.type.ts
@@ -1,6 +1,7 @@
 export interface DataAcademy {
   id?: string
   levelStudy?: string
+  title?: string
   otherStudy?: string
   otherLanguage?: string
   performance?: string
@@ -9,6 +10,7 @@ export interface DataAcademy {
 export const EmptyDataAcademyState: DataAcademy = {
   id: undefined,
   levelStudy: undefined,
+  title: undefined,
   otherStudy: undefined,
   otherLanguage: undefined,
   performance: undefined
