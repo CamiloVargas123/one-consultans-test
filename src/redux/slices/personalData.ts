9b42bc33b92ad6de663ed1799f73ea80a02b8401
Camilo Vargas <camilovargasingm@gmail.com>
2022-11-19 11:59:24 -0500

    rename extencion

---
+++ src/redux/slices/personalData.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { DataPersonal, EmptyDataPerosnalState } from 'src/models/dataPersonal.type';
+import { RootState } from '../store';
+
+export const personalData = createSlice({
+  name: 'personalData',
+  initialState: EmptyDataPerosnalState,
+  reducers: {
+    savePersonalData: (_, action: PayloadAction<DataPersonal>) => {
+      return action.payload;
+    },
+    modifyPersonalData: (state, action: PayloadAction<DataPersonal>) => {
+      return { ...state, ...action.payload }
+    },
+    resetPerosnalData: () => {
+      return EmptyDataPerosnalState
+    }
+  }
+})
+
+export const { savePersonalData, modifyPersonalData, resetPerosnalData } = personalData.actions;
+export const selectPersonalData = (state: RootState) => state.personalData
+export default personalData.reducer;
